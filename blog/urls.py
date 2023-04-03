864900f50caf254c971f3d8570232cccaba587c6
jaehyukpyon <jaehyuk.pyon@gmail.com>
2023-04-03 21:06:21 +0900

    2023/04/03

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.index),
+    # path('', views.index),
     path('', views.PostList.as_view()),
-    #path('<int:post_num>/', views.single_post_page)
+    # path('<int:post_num>/', views.single_post_page)
     path('<int:pk>/', views.PostDetail.as_view()),
 ]
