d566fdfa6e819a37560a352123fdb099167affee
jaehyukpyon <jaehyuk.pyon@gmail.com>
2023-04-18 16:34:58 +0900

    댓글 기능 및 post_detail.html에 보여주기 기능 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from markdownx.admin import MarkdownxModelAdmin
 
 # Register your models here.
@@ -21,3 +21,5 @@ class TagAdmin(admin.ModelAdmin):
     }
 
 admin.site.register(Tag, TagAdmin)
+
+admin.site.register(Comment)
